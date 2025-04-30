e9a977c08c900ff03c231c2f741295007c106609
ashishbaipalli23 <ashishbaipalli369@gmail.com>
2025-04-30 19:31:06 +0530

    dev chnaged

--- Git-javaproject1/src/main/java/com/ashi/gti/App.java
+++ Git-javaproject1/src/main/java/com/ashi/gti/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("this is TeamLead written");
+        System.out.println("this is TeamLead written");//hii
         System.out.println("this is developer written");
     }
 }
