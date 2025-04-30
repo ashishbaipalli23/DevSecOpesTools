c3f84c74b7e671ed2454025a82f29b7758b949ec
ashishbaipalli23 <ashishbaipalli369@gmail.com>
2025-04-30 19:06:05 +0530

    TL created Project

---
+++ Git-javaproject1/src/main/java/com/ashi/gti/App.java
@@ -0,0 +1,13 @@
+package com.ashi.gti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
